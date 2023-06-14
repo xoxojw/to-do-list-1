5563d1b831de8de270e03cb5fefc9dcc6d3e1f31
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-14 21:29:42 +0900

    :sparkles: 리스트에 To-do 항목 추가 기능 구현
    Input에 To-do 제목과 상세내용을 입력하고 추가하기 버튼이 누르면, todo list에 추가 되는 기능 구현. 현재는 console.log에서만 확인 가능하고 추가된 항목이 화면에 출력되지는 않음. useState Hook을 사용함

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import PageLayout from "components/PageLayout";
+import "styles/style.css";
+
+const App = () => {
+  return (
+    <>
+      <PageLayout></PageLayout>
+      <footer><p>React 개인과제(2023. 06) | 내일배움캠프 React 6기</p></footer>
+    </>
+  );
+}
+
+export default App;
