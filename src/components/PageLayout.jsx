5be36921af535067ccdd7dae97c17f0c56bfe5ff
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-15 21:02:15 +0900

    :sparkles: To-do의 진행 중, 완료 상태를 변경할 수 있는 기능 구현

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import Header from "./Header";
-import AddTodo from "./AddTodo";
-import ListTodo from "./ListTodo";
+import React, { useState } from "react";
+import Header from "components/Header";
+import AddTodo from "components/AddTodo";
+import ListTodo from "components/ListTodo";
 
-function PageLayout() {
+const PageLayout = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 0,
+      title: "리액트 과제",
+      detail: "재밌는데 아직 어렵다!!",
+      isDone: false,
+    },
+    {
+      id: 1,
+      title: "점심에 삼겹살 먹기",
+      detail: "직접 구워서 쌈싸먹기",
+      isDone: true,
+    },
+  ]);
+
   return (
     <main>
-      <Header></Header>
-      <AddTodo></AddTodo>
-      <ListTodo></ListTodo>
+      <Header />
+      <AddTodo todos={todos} setTodos={setTodos}/>
+      <ListTodo todos={todos} setTodos={setTodos} />
     </main>
   );
-}
+};
 
 export default PageLayout;
